7e25d0e9bd6ae0548330cf7e361a8771367c4a9e
DiogodePaula <depaulatrabalhos@gmail.com>
2020-08-08 00:27:16 -0300

    js

---
+++ src/addField.js
@@ -0,0 +1,20 @@
+
+document.querySelector("#add-time")
+    .addEventListener("click", cloneField)
+
+
+
+function cloneField() {
+    //duplicar os campos
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true);
+
+    const fields = newFieldContainer.querySelectorAll('input');
+
+    fields.forEach(function (field) {
+        field.value = '';
+    });
+
+    document.querySelector('#schedule-items').appendChild(newFieldContainer);
+}
+
+
